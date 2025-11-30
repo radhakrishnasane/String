1c4807982188bbd6cbf4dc5d3ef054ce2f3bcd7f
Radhakrishna <krishnaradha17@gmail.com>
2025-11-30 18:16:46 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,33 @@
+#string is a data type.A sequence of characters enclosed within a single cotes('') or double coats(" ") or triple coates
+(''' ''')
+#String operations:
+
+s=" Hi this is python string "
+#upper
+print(s.upper())
+#lower
+print(s.lower())
+#title and capitalize
+print(s.capitalize())
+print(s.title())
+#Strip(remove the space)
+print(s.strip())
+print(s.rstrip())
+print(s.lstrip())
+#replace
+b="i like java"
+print(b.replace('java','python'))
+#split
+c="i like pythonworld"
+print(c.split(" "))
+#join
+c=['a','b','c',]
+print(''.join(c))
+#swapcase
+s="Hello World"
+print(s.swapcase())
+
+
+
+
+
